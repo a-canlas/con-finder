dcea1274140cba5e6b0b4b4c1dfd7bd2b136c088
matchashiba <recr38@gmail.com>
2020-08-28 23:02:53 +0000

    Created database folder Added scripts for importing and exporting database Imported db to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+const db = require('./database');
 
 const app = express();
 
