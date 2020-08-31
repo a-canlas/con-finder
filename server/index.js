71292b2c0446a8eec999df3da3ee85b0e70593d6
matchashiba <recr38@gmail.com>
2020-08-31 23:47:33 +0000

    Added health-check for server Tested health-check api

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const app = express();
 
 app.use(staticMiddleware);
 
+app.get('/api/health-check', (req, res, next) => {
+  db.query('select \'successfully connected\' as "message"')
+    .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port ', process.env.PORT);
