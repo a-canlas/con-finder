b5608a7710fd95973a7267f929233905d6e83319
matchashiba <recr38@gmail.com>
2020-09-01 00:17:43 +0000

    Added api/all-conventions to server Queried database to grab all conventions upon get call to api/all-conventions

--- server/index.js
+++ server/index.js
@@ -13,6 +13,18 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/all-conventions', (req, res, next) => {
+  const sql = `SELECT "name", "website", "imagePath", "startDate", "endDate", "venueName", "address", "city", "state", "longitude", "latitude"
+  FROM conventions AS c
+  JOIN venues AS v
+  ON c."venueId" = v."venueId";`;
+  db.query(sql)
+    .then(result => {
+      res.status(200).json(result.rows);
+    })
+    .catch(err => next(err));
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port ', process.env.PORT);
