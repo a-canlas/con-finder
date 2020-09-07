9d561fbb384ab5308ea9b79c8c35b42cf7bf983f
matchashiba <recr38@gmail.com>
2020-09-07 19:01:37 +0000

    Added switch for determining month name Rearranged date from server to read as Month Day Year

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -47,16 +47,50 @@ class App extends React.Component {
   formatDate(dateString) {
     const index = dateString.indexOf('T');
     const splitDate = dateString.slice(0, i).split();
-    const splitMonth = splitDate[0];
+    const year = splitDate.shift();
+    const oldMonth = splitDate.shift();
+    splitDate.push(year);
     let month;
-    switch (month) {
+    switch (oldMonth) {
       case 1:
-        year = 'January';
+        month = 'January';
         break;
       case 2:
-        year = 'February';
+        month = 'February';
+        break;
+      case 3:
+        month = 'March';
+        break;
+      case 4:
+        month = 'April';
+        break;
+      case 5:
+        month = 'May';
+        break;
+      case 6:
+        month = 'June';
+        break;
+      case 7:
+        month = 'July';
+        break;
+      case 8:
+        month = 'August';
+        break;
+      case 9:
+        month = 'September';
+        break;
+      case 10:
+        month = 'October';
+        break;
+      case 11:
+        month = 'November';
+        break;
+      case 12:
+        month = 'December';
         break;
     }
+    splitDate.unshift(month);
+    return splitDate.join(' ');
 
   }
 
