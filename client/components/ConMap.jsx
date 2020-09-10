5e64e5cd414fed90025bbc39e97de71cf9c720fe
matchashiba <recr38@gmail.com>
2020-09-10 18:14:07 +0000

    Imported api key to ConMap Added google-maps-react Map component to ConMap

--- client/components/ConMap.jsx
+++ client/components/ConMap.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import api from '../../api';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
 
 class ConMap extends React.Component {
+
   render() {
     return (
-      <div>I&apos;m a map</div>
+      <Map
+        google={this.props.google}
+        zoom={13}
+        style={mapStyles}
+        initialCenter={{
+          latitude: 33.634870,
+          longitude: -117.740450
+        }}
+      />
     );
   }
 }
 
-export default ConMap;
+export default GoogleApiWrapper({
+  apiKey: api
+})(ConMap);
