e65ae7a19afd731f58d555649df2583d3d04277e
matchashiba <recr38@gmail.com>
2020-09-10 17:39:16 +0000

    Created initial component to hold Google Map

---
+++ client/components/ConMap.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+class ConMap extends React.Component {
+  render() {
+    return (
+      <div>I&apos;m a map</div>
+    );
+  }
+}
+
+export default ConMap;
