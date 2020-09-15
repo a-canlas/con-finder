baaa8fa2f77743335fc9638ecda450f7bd60e5e1
matchashiba <recr38@gmail.com>
2020-09-15 18:48:56 +0000

    Added onClick to ViewButton Added variant property to View Button Passed props to Button text

--- client/components/ViewButton.jsx
+++ client/components/ViewButton.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 const ViewButton = props => {
+  const view = props.viewName;
   return (
-    <Button />
+    <Button variant="info" onClick={() => props.toggleView(view)}>{props.viewName}</Button>
   );
 };
 
