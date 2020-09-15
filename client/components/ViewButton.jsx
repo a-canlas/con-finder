22891306afe19063b164471b004da7bb7a12f094
matchashiba <recr38@gmail.com>
2020-09-15 19:23:17 +0000

    Appended 'View' to ViewButton text

--- client/components/ViewButton.jsx
+++ client/components/ViewButton.jsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 const ViewButton = props => {
   const view = props.viewName;
   return (
-    <Button variant="info" onClick={() => props.toggleView(view)}>{view === 'list' ? 'List' : 'Map'}</Button>
+    <Button variant="info" onClick={() => props.toggleView(view)}>{view === 'list' ? 'List View' : 'Map View'}</Button>
   );
 };
 
